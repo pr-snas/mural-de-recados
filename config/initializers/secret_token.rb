86e5ca29977f62c8cf5794d1278e0a8ed746fda0
Marcos Lopes <marcosmlopes01@gmail.com>
2012-06-04 09:11:43 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuralDeRecados::Application.config.secret_token = '0ff79bf7a71863f4caf8a6abafed16636666001ea5c9f26399aefb86cbcee169ed39c93741a8b088914ab0347be1d8117b13f404463310324c646bd64a28c6d9'
