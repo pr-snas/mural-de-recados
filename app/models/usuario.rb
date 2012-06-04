dbc0d4015fb913f3b1dd7119075488956408345e
Marcos Lopes <marcosmlopes01@gmail.com>
2012-06-04 10:18:11 -0300

    Adicionando classe ability, validando papel e nome do model usuario

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -6,6 +6,12 @@ class Usuario < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :nome, :papel, :email, :password, :password_confirmation, :remember_me
+
+  validates :papel, inclusion: { in: %w(admin moderador usuario) }
+  validates :nome, presence: true
+
+  def admin?
+    self.papel == 'admin'
+  end
 end
