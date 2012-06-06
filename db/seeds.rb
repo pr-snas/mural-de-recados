719b4132d4f178ac234179b06c16409e0b49dfb3
Marcos Lopes <marcosmlopes01@gmail.com>
2012-06-06 03:27:49 -0300

    Corrigindo dados dos models criados no db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 usuario = Usuario.create(nome: 'Marcos Lopes', email: 'marcosmlopes01@gmail.com',
                          password: 'secret', papel: 'admin')
 Recado.create(conteudo: 'Lorem ipsum dolor sit amet', usuario: usuario,
-              top: 54, left: 13, z_index: 1, width: 210, height: 210,
-              background_color: 'red', font_family: 'Helvetica')
+              top: 21, left: 36, z_index: 1, width: 210, height: 210, pin_class: 'red',
+              background_color: 'red', font_family: 'Helvetica', text_color: '#000')
 Recado.create(conteudo: 'Lorem ipsum dolor sit amet', usuario: usuario,
-              top: 110, left: 170, z_index: 2, width: 300, height: 200,
-              background_color: '#ffffff', font_family: 'Arial')
+              top: 25, left: 302, z_index: 2, width: 300, height: 200, pin_class: 'black',
+              background_color: '#ffffff', font_family: 'Arial', text_color: '#000')
 Recado.create(conteudo: 'Lorem ipsum dolor sit amet', usuario: usuario,
-              top: 290, left: 90, z_index: 3, width: 200, height: 150,
-              background_color: 'blue', font_family: 'Arial')
+              top: 16, left: 645, z_index: 3, width: 200, height: 150, pin_class: 'green',
+              background_color: 'blue', font_family: 'Arial', text_color: '#fff')
 Recado.create(conteudo: 'Lorem ipsum dolor sit amet', usuario: usuario,
-              top: 80, left: 990, z_index: 4, width: 280, height: 180,
-              background_color: 'yellow', font_family: 'Arial')
+              top: 20, left: 898, z_index: 4, width: 280, height: 180, pin_class: 'blue',
+              background_color: 'yellow', font_family: 'Arial', text_color: '#000')
 Recado.create(conteudo: 'Lorem ipsum dolor sit amet', usuario: usuario,
-              top: 120, left: 510, z_index: 5, width: 500, height: 400,
-              background_color: '#ccc', font_family: 'Arial')
+              top: 265, left: 426, z_index: 5, width: 500, height: 400, pin_class: 'red',
+              background_color: '#ccc', font_family: 'Arial', text_color: '#000')
