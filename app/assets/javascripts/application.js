945f94de718312bdac8c1f7a2c45bf5a84cae06d
Marcos Lopes <marcosmlopes01@gmail.com>
2012-07-03 14:43:11 -0300

    Incluindo lib spin.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require spin
 //= require jquery
 //= require jquery_ujs
 //= require underscore
