6064964bb6be58d5f3f8c303399f79cc1f592e06
Marcos Lopes <marcosmlopes01@gmail.com>
2012-07-09 14:50:20 -0300

    Atualizando nome da tabela categorias no db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120709170313) do
+ActiveRecord::Schema.define(:version => 20120709173846) do
 
-  create_table "categoria", :force => true do |t|
+  create_table "categorias", :force => true do |t|
     t.string   "nome"
     t.string   "background_color"
     t.datetime "created_at",       :null => false
